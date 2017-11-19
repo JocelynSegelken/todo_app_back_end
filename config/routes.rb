657ee8934a25c546259196f7ad1afaf8cfde426e
Jocelyn Segelken <jocelynsegelken@gmail.com>
2017-11-19 19:33:00 +0000

    Add the RCAV flows for the new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
     get 'todo/index', to: 'todo#index'
+    get 'todo/create', to: 'todo#create'
     get 'todo/show/:id', to: 'todo#show'
+    get 'todo/new', to: 'todo#new'
 end
